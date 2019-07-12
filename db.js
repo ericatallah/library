03b603aa8d85e1d0bdb62f4008683abf75cd22f8
Eric Atallah <eric.atallah@onclickwebdesign.com>
2019-07-12 11:47:42 -0700

    Add a handle disconnect function to DB connection.

--- db.js
+++ db.js
@@ -2,20 +2,52 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 // Create connection
-const db = mysql.createConnection({
+let db;/* = mysql.createConnection({
     host     : process.env.DB_CONN_HOST,
     user     : process.env.DB_CONN_USER,
     password : process.env.DB_CONN_PW,
     database : process.env.DB_CONN_DBNAME,
     multipleStatements: true
-});
+});*/
 
 // Connect
-db.connect((err) => {
+const handleDisconnect = () => {
+    db = mysql.createConnection({
+        host     : process.env.DB_CONN_HOST,
+        user     : process.env.DB_CONN_USER,
+        password : process.env.DB_CONN_PW,
+        database : process.env.DB_CONN_DBNAME,
+        multipleStatements: true
+    });
+
+    db.connect((err) => {
+        if(err) {
+            console.log('Error when connecting to db: ', err);
+            setTimeout(handleDisconnect, 2000);
+            //throw err;
+        }
+        console.log('MySql Connected...');
+    });
+
+    db.on('error', err => {
+        console.log('DB error: ', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    })
+};
+
+handleDisconnect();
+
+/*db.connect((err) => {
     if(err){
-        throw err;
+        console.log('Error when connecting to db: ', err);
+        setTimeout(handleDisconnect, 2000);
+        //throw err;
     }
     console.log('MySql Connected...');
-});
+});*/
 
 module.exports = db;
